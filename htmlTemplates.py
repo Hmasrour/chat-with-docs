4da10b4351e24b1f3a669594068c4f049b6c78f5
unknown <hamzamasrour85@gmail.com>
2024-08-20 16:16:06 +0100

    bot and user bot images modified

--- htmlTemplates.py
+++ htmlTemplates.py
@@ -25,20 +25,20 @@ css = '''
 }
 '''
 
+user_template = '''
+<div class="chat-message user">
+    <div class="avatar">
+        <img src="https://img.freepik.com/free-vector/illustration-user-avatar-icon_53876-5907.jpg">
+    </div>    
+    <div class="message">{{MSG}}</div>
+</div>
+'''
 bot_template = '''
 <div class="chat-message bot">
     <div class="avatar">
-        <img src="https://static.vecteezy.com/system/resources/previews/010/054/157/original/chat-bot-robot-avatar-in-circle-round-shape-isolated-on-white-background-stock-illustration-ai-technology-futuristic-helper-communication-conversation-concept-in-flat-style-vector.jpg" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
+        <img src="https://img.freepik.com/free-vector/graident-ai-robot-vectorart_78370-4114.jpg" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
     </div>
     <div class="message">{{MSG}}</div>
 </div>
 '''
 
-user_template = '''
-<div class="chat-message user">
-    <div class="avatar">
-        <img src="https://static.vecteezy.com/system/resources/previews/014/212/681/non_2x/female-user-profile-avatar-is-a-woman-a-character-for-a-screen-saver-with-emotions-for-website-and-mobile-app-design-illustration-on-a-white-isolated-background-vector.jpg">
-    </div>    
-    <div class="message">{{MSG}}</div>
-</div>
-'''
